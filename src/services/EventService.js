ac7a50e8408a6d5e6ed2fb326b845bc439a066cb
Hachikuya <ina120533@gmail.com>
2020-05-20 15:46:55 +0800

    Fix prettier errors and add devlog.md

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: 'http://localhost:3000',
   withCredentials: false,
   headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
   }
-});
+})
 
 export default {
   getLikesList() {
-    return apiClient.get("/likes_info");
+    return apiClient.get('/likes_info')
   }
-};
+}
